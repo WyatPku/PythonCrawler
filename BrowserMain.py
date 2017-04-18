f168b19282201589ad6f9fa9d95b9035d24d5959
WyatPku <1600012704@pku.edu.cn>
2017-04-18 19:19:54 +0800

    登入校园网并实现跳转页面

--- BrowserMain.py
+++ BrowserMain.py
@@ -3,6 +3,7 @@
 
 import time
 from splinter import Browser
+from pkulinks import *
 
 
 def main():
@@ -11,16 +12,23 @@ def main():
     username = infile.readline()
     password = infile.readline()
     infile.close()
-    url = "https://iaaa.pku.edu.cn/iaaa/oauth.jsp?appID=syllabus&appName=学生选课系统" + \
-          "&redirectUrl=http://elective.pku.edu.cn:80/elective2008/agent4Iaaa.jsp/../ssoLogin.do"
+    url = Links.login()
     browser = Browser('firefox')
     browser.visit(url)
-    time.sleep(3)
+    # time.sleep(3)
     browser.find_by_id('user_name').fill(username)
     browser.find_by_id('password').fill(password)
     browser.find_by_id('submit_button').click()
-    time.sleep(8)
-
+    time.sleep(1)
+    browser.click_link_by_href(Links.xuankejieguo())
+    # time.sleep(8)
+    # 通过命令行来控制抓取
+    print "ready to catch information!"
+    cmd = raw_input()
+    while cmd != "exit":
+        if cmd.startswith("get "):
+            print "begin catching information"
+        cmd = raw_input()
     browser.quit()
 
 if __name__ == '__main__':
---
+++ pkulinks.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# using python 2.7
+
+
+class Links:
+    def __init__(self):
+        print "init electiveLinks"
+
+    @staticmethod
+    def login():
+        link = "https://iaaa.pku.edu.cn/iaaa/oauth.jsp?appID=syllabus&appName=学生选课系统" + \
+               "&redirectUrl=http://elective.pku.edu.cn:80/elective2008/agent4Iaaa.jsp/../ssoLogin.do"
+        return link
+
+    @staticmethod
+    def xuankejihua():
+        return "/elective2008/edu/pku/stu/elective/controller/electivePlan/ElectivePlanController.jpf"
+
+    @staticmethod
+    def yuxuan():
+        return "/elective2008/edu/pku/stu/elective/controller/electiveWork/ElectiveWorkController.jpf"
+
+    @staticmethod
+    def xuankejieguo():
+        return "/elective2008/edu/pku/stu/elective/controller/electiveWork/showResults.do"
